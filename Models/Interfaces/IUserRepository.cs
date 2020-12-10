11aa0fd3db137eda61de282ec856efd5189a9e18
Jakub Langner <jakub.langner2@gmail.com>
2020-12-10 15:49:27 +0100

    added user repository

---
+++ Models/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeService.Models.Interfaces
+{
+    public interface IUserRepository
+    {
+        public List<AppUser> Users();
+        public List<IdentityRole> Roles();
+        public void AddUser(AppUser user);
+        public void EditUser(AppUser user);
+        public void DeleteUser(AppUser user);
+    }
+}
---
+++ Models/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using BikeService.Models.Database;
+using BikeService.Models.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace BikeService.Models.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DatabaseContext _context;
+        public UserRepository(DatabaseContext context, SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _context = context;
+        }
+
+        public void AddUser(AppUser user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public void DeleteUser(AppUser user)
+        {
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+        }
+
+        public void EditUser(AppUser user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+        public List<IdentityRole> Roles()
+        {
+            return _context.Roles.ToList();
+        }
+
+        public List<AppUser> Users()
+        {
+            return _context.Users.ToList();
+        }
+
+    }
+}
